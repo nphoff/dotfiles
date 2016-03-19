80259335e321c04e8256995007c2588109838c9b
Nathan Hoffman <hoffman.nathan89@gmail.com>
2016-03-19 09:44:23 -0400

    I am bad at bash

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,4 +1,19 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # install vundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
+#git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
+
+#simlink files
+#This'll have to be run from the dotfiles directory.
+
+dir=$(pwd);
+
+for file in .tmux.conf .zshrc .vimrc.plugins .vimrc .xinitrc
+do
+
+if [ -e ~/$file ]; then
+    mv ~/$file ~/"${file}.old"
+fi
+ln -s "${dir}/$file" "$HOME/$file";
+
+done
