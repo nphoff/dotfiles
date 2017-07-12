dde441e86394677aeec0cbd09e4e689947a99157
Nathan Hoffman <nhoffman@Nathans-MacBook.local>
2017-07-12 09:09:20 -0400

    updating setup script

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
-
-# install vundle
-#git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
-
-#simlink files
 #This'll have to be run from the dotfiles directory.
 
 dir=$(pwd);
 
+# install vundle
+if ! [ -e ~/.vim/bundle/Vundle.vim ]; then
+    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
+fi
+
+# install vim plug
+if ! [ -e ~/.vim/autoload/plug.vim ]; then
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
+fi
+
+# copy over all the rc files
 for file in .tmux.conf .zshrc .vimrc.plugins .vimrc .xinitrc
 do
 
 if [ -e ~/$file ]; then
     mv ~/$file ~/"${file}.old"
 fi
-ln -s "${dir}/$file" "$HOME/$file";
+cp "${dir}/$file" "$HOME/$file";
 
 done
